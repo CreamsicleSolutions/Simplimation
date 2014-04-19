ba7929a7fca76b6bc37914ddae835a1d0ba77589
Chris (Sunshine) <punxzero@hotmail.com>
2014-04-19 12:08:30 -0400

    0.0.61
    This version has .avi export working however, it requires that each
    frame is the same delay.

---
+++ 0.0.61 (4-19-14)/Simplimation/bin/Debug/AviWriter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using AForge.Video.VFW;
+using AviFile;
+
+namespace Simplimation
+{
+    class AviWriter
+    {
+        // private variables for Avi
+        private List<double> del = new List<double>();
+        private List<Bitmap> bitmaps = new List<Bitmap>();
+        private double min;
+        private int height = 480;
+        private int width = 848;
+        private double opfr;
+
+        // constructor
+        public AviWriter(List<frame> Frame, String filename)
+        {
+            Console.WriteLine("Frame.Count: {0}", Frame.Count);
+            string path = Environment.CurrentDirectory + "\\Images\\169.png";
+            Image blank = Image.FromFile(path);
+            Bitmap blankbuf = new Bitmap(blank, width, height);
+
+            bitmaps.Add(blankbuf);
+
+            del.Add(0.1);
+
+            for (int i = 0; i < Frame.Count; i++)
+            {
+                del.Add((((double)Frame[i].delay) / 1000.00));
+                string FileName  = Frame[i].src;
+                Image img = Image.FromFile(FileName);
+                Bitmap bitmap = new Bitmap(img, width, height);
+                bitmaps.Add(bitmap);
+
+                Console.WriteLine("Frame: {0}", i, " delay: {1}", ((double)Frame[i].delay / 1000.00), "src: {2}", Frame[i].src);
+            }
+
+            min = del.Min();
+            opfr = 1000 / (min * 1000);
+            AviManager aviManager = new AviManager(filename, false);
+            VideoStream aviStream = aviManager.AddVideoStream(false, opfr, bitmaps[0]);
+            Console.WriteLine("min: {0}", min);
+            
+
+            
+
+
+            for(int i = 0; i < bitmaps.Count; i++)
+            {
+
+                if (del[i] > min)
+                {
+                    double j = del[i];
+                    while ( j > min)
+                    {
+                        aviStream.AddFrame(bitmaps[i]);
+                        j = j - min;
+                    }
+
+                }
+                else
+                {
+                    aviStream.AddFrame(bitmaps[i]);
+                }
+
+                
+                //Console.WriteLine("Frame added");
+             /*   
+                for(double j = min; j <= del[i];)
+                {
+                    aviStream.AddFrame(bitmaps[i]);
+                    j = j + min;
+
+                    Console.WriteLine("j: {0}", j);
+                }
+              */
+            }
+
+            aviManager.Close();
+            del.Clear();
+        }
+
+        // methods
+        double FindMin(List<double> values) { return values.Min(); }
+
+        bool MilliSecondsToSeconds(ref List<double> ms)
+        {
+            return true;
+        }
+
+        double MilliSecondsToSecond(ref double ms) { return (ms / 1000); }
+
+    }
+}
